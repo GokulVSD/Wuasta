7b8945a097d98aa4fa66e7ad1205cb1c71904422
GokulVSD <gokul.vasda@gmail.com>
2017-09-17 03:16:50 +0530

    Created new collapsable ActionBar and added high res artwork

--- app/src/main/java/com/walarm/wuasta/MainActivity.java
+++ app/src/main/java/com/walarm/wuasta/MainActivity.java
@@ -1,7 +1,9 @@
 package com.walarm.wuasta;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
