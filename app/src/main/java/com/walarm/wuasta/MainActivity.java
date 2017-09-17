4fa42bac4a61a9601ddb87d2b498e16463f1db59
GokulVSD <gokul.vasda@gmail.com>
2017-09-17 19:39:51 +0530

    Added new BottomNavBar, added animations, added artwork

--- app/src/main/java/com/walarm/wuasta/MainActivity.java
+++ app/src/main/java/com/walarm/wuasta/MainActivity.java
@@ -1,9 +1,18 @@
 package com.walarm.wuasta;
 
+import android.graphics.Typeface;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenu;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.SpannableStringBuilder;
+import android.text.style.TypefaceSpan;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +20,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView mBottomNav = (BottomNavigationView) findViewById(R.id.navigation);
+
+        mBottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+
+                ((TextView)findViewById(R.id.temptext)).setText(item.getTitle().toString()+": Try scrolling m8");
+
+                return true;
+            }
+        });
+
     }
 }
